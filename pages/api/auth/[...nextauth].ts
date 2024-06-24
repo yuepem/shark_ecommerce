9bfad3de325e2c35dc0a6ff48c4a4fc081c8992d
x_yue <103266340+yuepem@users.noreply.github.com>
2024-06-25 00:58:00 +0200

    fix qpi request 404 by export default funxtion

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -6,12 +6,14 @@ import { PrismaClient } from "@prisma/client"
 
 const prisma = new PrismaClient()
 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+
+export default NextAuth({
+
     adapter: PrismaAdapter(prisma),
     providers: [
         GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+            clientId: process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
         // add github later
         /* GithubProvider({
@@ -19,4 +21,4 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
             clientSecret: process.env.GITHUB_SECRET,
         }) */
     ],
-})
+}) 
