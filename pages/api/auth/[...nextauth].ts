213638fb0f86f205f1f357dc5d780ef7ecf71aa6
x_yue <103266340+yuepem@users.noreply.github.com>
2024-07-08 10:58:12 +0200

    update development information

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -25,7 +25,7 @@ export const authOptions = ({
     ],
 
     events: {
-        createUser: async ({ user }) => {
+        createUser: async ({ user }: any) => {
             const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
                 apiVersion: "2024-06-20",
             })
